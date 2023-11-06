33daea93cf5163c8697a1c629eaee0a3fabe3de9
Hanzala-Ahmed <hanzala.ahmed181@gmail.com>
2023-11-06 23:02:40 +0500

    fix:timelineIssue

--- src/components/DetailPage/TimeLine/TimeLine.jsx
+++ src/components/DetailPage/TimeLine/TimeLine.jsx
@@ -1,6 +1,5 @@
 import { timelineList } from "@/constants";
 import Timeline from "@mui/lab/Timeline";
-import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -12,49 +11,48 @@ import { NumberConatiner, TimeLineItemNoText } from "./style";
 export default function TimeLine() {
   const themeMode = useSelector((state) => state.theme.themeMode);
   const renderItem = (item, index) => {
-    const isSeparator = index > 0 && index < 5;
     return (
-      <TimelineItem key={index}>
-        <TimelineSeparator style={{ width: 100, backgroundColor: "#E4DCFD" }}>
-          <TimelineConnector />
-          <NumberConatiner>
-            <TimeLineItemNoText>{item.title}</TimeLineItemNoText>
-          </NumberConatiner>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Box
-            border={"1px solid gray"}
-            padding={4}
-            borderRadius={5}
-            width={400}
-          >
-            <Typography
-              variant="h2"
-              fontWeight={"bold"}
-              fontSize={10}
-              color={themeMode == "light" ? "#000" : "#fff"}
+      <>
+        <TimelineItem key={index}>
+          <TimelineSeparator style={{ width: 100, backgroundColor: "#E4DCFD" }}>
+            <NumberConatiner>
+              <TimeLineItemNoText>{item.title}</TimeLineItemNoText>
+            </NumberConatiner>
+          </TimelineSeparator>
+          <TimelineContent>
+            <Box
+              border={"1px solid gray"}
+              padding={4}
+              borderRadius={5}
+              width={400}
             >
-              {item.descriptionTitle}
-            </Typography>
-            <Typography
-              variant="h2"
-              mt={0.5}
-              fontSize={10}
-              color={themeMode == "light" ? "#000" : "#fff"}
-            >
-              {item.description}
-            </Typography>
-          </Box>
-        </TimelineContent>
-      </TimelineItem>
+              <Typography
+                variant="h2"
+                fontWeight={"bold"}
+                fontSize={10}
+                color={themeMode === "light" ? "#000" : "#fff"}
+              >
+                {item.descriptionTitle}
+              </Typography>
+              <Typography
+                variant="h2"
+                mt={0.5}
+                fontSize={10}
+                color={themeMode === "light" ? "#000" : "#fff"}
+              >
+                {item.description}
+              </Typography>
+            </Box>
+          </TimelineContent>
+        </TimelineItem>
+      </>
     );
   };
   return (
     <>
       <Timeline
         sx={{
-          width: "100%",
+          width: "85%",
         }}
         position="alternate"
       >
